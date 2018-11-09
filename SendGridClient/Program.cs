bb11b468f9f5c722eaf3750e39d3bf13b35a4f26
Troy Reeves <troyreeves2@gmail.com>
2018-11-09 10:22:41 -0700

    Sendgrid implemented and working.

---
+++ SendGridClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
+using dotenv.net;
+
+namespace PRPC_1
+{
+    class Program
+    {
+        internal class Example
+    {
+        
+        private static void Main()
+        {
+            
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            DotEnv.Config();
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("troyreeves2@gmail.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("troyreeves2@gmail.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
+}
---
+++ SendGridLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PRPC_1
+{
+    public class Class1
+    {
+    }
+}
