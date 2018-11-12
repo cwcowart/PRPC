8538edc0aac43ad008828be3234deaf4387dadf1
Troy Reeves <troyreeves2@gmail.com>
2018-11-11 17:32:37 -0700

    Created  solution file and created the sendgrid controller.

--- SendGridLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PRPC_1
-{
-    public class Class1
-    {
-    }
-}
---
+++ SendGridLib/SendGrid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
+using dotenv.net;
+
+namespace PRPC_1
+{
+    public class Class1
+    {
+        interface ISMTP
+        {
+            string EmailAddress(string ConfEmail);
+        }
+
+        public class EmailConf:ISMTP
+        {
+            public string EmailAddress (string ConfEmail)
+            {
+                VerifyEmail();
+            }
+        }        
+    }
+}
+
